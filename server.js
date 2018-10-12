36ece88a1a1cd6d34ea45765ea22c7ddf5f554eb
AlassaneDiop <diopad@ept.sn>
2018-10-12 12:55:09 +0000

    commit 2

--- server.js
+++ server.js
@@ -2,31 +2,21 @@
 const express = require('express');
 const path = require('path');
 
-const Cors = require('cors');
 const app = express();
 
-app.use(Cors());
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  });
+  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/pbiadnpoc'));
-
-/*
-// set up CORS resource sharing
-app.use(function(req, res, next){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, Accept, X-Requested-With');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-})*/
-
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
   res.sendFile(path.join(__dirname+ '/dist/pbiadnpoc/index.html'));
 });
 // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
-
 app.listen(process.env.PORT || 8080);
