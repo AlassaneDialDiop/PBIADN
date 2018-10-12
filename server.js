893e9288e120cf7e81caf6c66b4d4bbbefc2cc50
AlassaneDiop <diopad@ept.sn>
2018-10-12 12:24:49 +0000

    Second commit

--- server.js
+++ server.js
@@ -8,9 +8,22 @@ const app = express();
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/pbiadnpoc'));
 
+// set up CORS resource sharing
+app.use(function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+})
+
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
   res.sendFile(path.join(__dirname+ '/dist/pbiadnpoc/index.html'));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
+
+var server = app.listen(process.env.PORT || 5000, function(){
+  console.log('App listeining on', server.address().port);
+})
