6d8d9ae521f84b3787234f2e53cfe8aba1df8c56
AlassaneDiop <diopad@ept.sn>
2018-10-12 12:18:38 +0000

    Third commit

--- src/app/interceptor.module.ts
+++ src/app/interceptor.module.ts
@@ -2,7 +2,7 @@ import { Injectable, NgModule} from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse} from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import 'rxjs/add/operator/do';
+//import 'rxjs/add/operator/do';
 @Injectable()
 export class HttpsRequestInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
